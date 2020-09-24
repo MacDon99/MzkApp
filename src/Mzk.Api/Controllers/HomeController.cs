a558f3043f8c7754aba012e4f8f2b70548a2242a
MacDon99 <donczyk.maciej@gmail.com>
2020-09-24 15:22:38 +0200

    created projects from templates

---
+++ src/Mzk.Api/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MzkApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
